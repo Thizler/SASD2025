e352642c7a9143c786223c5690d6a9d4d9d4ce3e
thizler <may99923@gmail.com>
2026-02-06 16:22:08 +0700

    Class Design Lab

---
+++ ClassDesign/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDesign;
+
+public class Product
+{
+    public required string Name { get; init; }
+    public required int Quantity { get; init; }
+    public required decimal Price { get; init; }
+
+    // Todo#1: create computed property "TotalPrice" as decimal
+    public decimal TotalPrice => Quantity * Price;
+
+    // Todo#2: override ToString()
+    public override string ToString()
+    {
+        return $"Product: {Name}, Quantity: {Quantity}, Price: {Price:C}, Total: {TotalPrice:C}";
+    }
+}
